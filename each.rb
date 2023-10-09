449698a80483398e3e6a683387b146a82329e3a8
Barry Allahyar <barrya@gmail.com>
2023-10-08 20:07:59 -0500

    Update m0_be_iteration with answers.

--- each.rb
+++ each.rb
@@ -4,11 +4,37 @@
   
 # Challenge 1
 # Use the each method to print out a complete sentence stating the weather using the Array below:
+
+# declare a variable weather_options that stores an Array of 5 Strings
 weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+
+# Call the each method on weather_options Array
+# Use "weather" as variable name for each element as it's being iterated over
+weather_options.each do |weather|
+  # puts an interpolated sentence using the current value of weather
+  puts "The current weather is #{weather}."
+# end the each code block  
+end
+
+# The each block will run 5 times because there are 5 elements in the Array
+# The first time it runs, the String "sunny" is the value of the weather variable
+# The second time it runs, the String "snowy" is the value of the weather variable
+# The third time it runs, the String "freezing" is the value of the weather variable
+# The fourth time it runs, the String "party cloudy" is the value of the weather variable
+# The fifth time it runs, the String "drizzly" is the value of the weather variable
+# Once it's run on every element in the Array, it reads the end keyword, and is finished
 
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
+words.each do |word|
+  if word.length == 3
+    puts word
+  end
+end
+
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
+
+# https://www.loom.com/share/f0dcfe9673074f019a550e79f59fd473?sid=952488b0-fa4e-421e-8ec2-e35c62b8680c
--- self-evaluation.md
+++ self-evaluation.md
@@ -1,6 +1,13 @@
 # Self Evaluation
 
-- How might the `each` method be useful?
-- What do you still need to practice or learn? How will you do that?
-- What worked well for you in terms of your approach on this
+1. How might the `each` method be useful?
+> The 'each' method can be useful in many different ways, one such useful use of the 'each' method could be retrieving multiple elements (a specific subset) within the array beyond just '.first' or .last' and method for example. 
+> Also, it could be used to getting a random entry from an Array. For example, I have a friend building a program on calling students within a gradeschool classroom that is completely randomized. The each method could be used to randomly choose a student within an array.
+
+2. What do you still need to practice or learn? How will you do that?
+Learning about the many different uses and applications of how the 'each' method could be used, as well as gaining a decent basic understanding of it's use by practice using it. 
+
+
+3. What worked well for you in terms of your approach on this
 assignment?
+> Walking through the assignment and code step-by-step and understanding (or guessing beforehand) what exactly the code is supposed to do.
